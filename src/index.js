7ec3b2acfcda92541b282336f593f94b71c389c5
akiijadhav <akhilesh.jadhav.j@softwaregroup.com>
2022-12-27 18:26:43 +0530

    commenting configs

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://odyssey-lift-off-server-production-7e54.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
+  // name: "web",
+  // version: "1.0",
 });
 
 ReactDOM.render(
